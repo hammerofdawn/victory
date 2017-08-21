0634f8559e63f554c16749eb2b4e1e8145d190c0
Denis Smajlović <denis@smajlovic.dk>
2017-08-21 16:16:02 +0200

    Add models.

--- staff/admin.py
+++ staff/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
 # Register your models here.
-from .models import ExtendedUser, Team, DriversLicenceCategories, Language, Article
+from .models import ExtendedUser, Team, DriversLicenceCategories, Language, Article, TShirt, Sock, Review, Alert
 
 class UserInline(admin.StackedInline):
     model = ExtendedUser
@@ -24,3 +24,7 @@ admin.site.register(Team, TeamAdmin)
 admin.site.register(DriversLicenceCategories)
 admin.site.register(Language)
 admin.site.register(Article)
+admin.site.register(TShirt)
+admin.site.register(Sock)
+admin.site.register(Review)
+admin.site.register(Alert)
