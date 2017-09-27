c147cfb9f60cee43852cb2ed9b2094be19000cb7
Meicki <melonendk@gmail.com>
2017-09-27 16:33:51 +0200

    Added list view for teams and users & added it to dark theme.

---
+++ staff/static/js/custom.js
@@ -0,0 +1,13 @@
+function listview() {
+		$("#browseview").hide();
+		$("#listviewbtn").hide();
+		$("#browseviewbtn").show();
+		$("#listviewfield").show();
+}
+
+function browseviewbtn() {
+		$("#listviewfield").hide();
+		$("#browseviewbtn").hide();
+		$("#listviewbtn").show();
+		$("#browseview").show();
+}
