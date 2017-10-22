b4e68fc08fd86e3d66bfa4257f0e85b23387519a
Denis Smajlović <denis@smajlovic.dk>
2017-10-22 22:14:04 +0200

    Fix loads of things

--- staff/views.py
+++ staff/views.py
@@ -173,30 +173,6 @@ def usersettings(request, user_pk):
 
 	return render(request, 'user/settings.html', context)
 
-@login_required
-def userdescription(request, user_pk):
-	logged_in_user = get_object_or_404(User, pk=request.user.pk)
-	requested_user = get_object_or_404(User, pk=user_pk)
-
-	context = {
-		'logged_in_user': logged_in_user,
-		'requested_user': requested_user,
-	}
-
-	return render(request, 'user/description.html', context)
-
-@login_required
-def userimage(request, user_pk):
-	logged_in_user = get_object_or_404(User, pk=request.user.pk)
-	requested_user = get_object_or_404(User, pk=user_pk)
-
-	context = {
-		'logged_in_user': logged_in_user,
-		'requested_user': requested_user,
-	}
-
-	return render(request, 'user/images.html', context)
-
 def team(request, team_pk):
 	requested_team = get_object_or_404(Team, pk=team_pk)
 
